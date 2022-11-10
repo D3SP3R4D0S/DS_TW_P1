ee9991321918c24ac7316be09465acc79020b560
D3SP3R4D0S <90773781+D3SP3R4D0S@users.noreply.github.com>
2022-11-10 22:41:00 +0900

    [feat] apply moving algorithm

--- main.py
+++ main.py
@@ -16,7 +16,7 @@
 # 이를 통해 총 5번의 게임이 이루어졌을 때 시도횟수와 사용자이름을 기록하여 이를 정렬하여 보여주어라.
 #
 # 제출자료 : 아이디어에 대한 간단한 정리문서(word 혹은 hwp), 프로그램코드
-
+import time
 import turtle as t
 import random
 
@@ -96,7 +96,101 @@ def getname():
 def targetmove():
     x = target.xcor()
     y = target.ycor()
-    print(x, y)
+    # 알고리즘 구성
+    # 4분면으로 나눈다 각 0~50 50~100 씩 x y 좌표로 할당
+    # 사용자가 각 사분면에 있을때의 행동이 변화한다
+    # 사용자가 반대 되는 사분면에 있을경우 벽쪽으로 도망
+    # 사용자가 맞은편 사분면에 있을경우 반대편 사분면으로 도망
+
+    x_to_user = x - user.xcor()
+    y_to_user = y - user.ycor()
+    # x to user + y to user +  동북쪽, case1
+    # x to user + y to user -  동남쪽, case2
+    # x to user - y to user +  서북쪽, case3
+    # x to user - y to user -  서남쪽, case4
+
+    if x == 100 and y == 100:
+        if x_to_user > y_to_user:
+            target.setx(x-10)
+        else:
+            target.sety(y-10)
+    elif x == 0 and y == 0:
+        if x - user.xcor() > y - user.ycor():
+            target.setx(x+10)
+        else:
+            target.sety(y+10)
+        # move x or move y -
+    elif x == 100:
+        # move y or move x -
+        if abs(x_to_user) > abs(y_to_user):
+            target.setx(x-10)
+        elif y_to_user > 0:
+            target.sety(y+10)
+        elif y_to_user < 0:
+            target.sety(y-10)
+    elif y == 100:
+        # move x or move y -
+        if abs(y_to_user) > abs(x_to_user):
+            target.sety(y-10)
+        elif x_to_user > 0:
+            target.setx(x+10)
+        elif x_to_user < 0:
+            target.setx(x-10)
+    elif x == 0:
+        if abs(x_to_user) > abs(y_to_user):
+            target.setx(x+10)
+        elif y_to_user > 0:
+            target.sety(y+10)
+        elif y_to_user < 0:
+            target.sety(y-10)
+        # move y or move x -
+    elif y == 0:
+        if abs(y_to_user) > abs(x_to_user):
+            target.sety(y-10)
+        elif x_to_user > 0:
+            target.setx(x+10)
+        elif x_to_user < 0:
+            target.setx(x-10)
+        # move y or move x -
+    else:
+        if x_to_user > 0:
+            if y_to_user > 0:
+                if abs(y_to_user) > abs(x_to_user):
+                    target.setx(x + 10)
+                else:
+                    target.sety(y + 10)
+            else:
+                if abs(y_to_user) > abs(x_to_user):
+                    target.setx(x + 10)
+                else:
+                    target.sety(y - 10)
+        elif x_to_user < 0:
+            if y_to_user > 0:
+                if abs(y_to_user) > abs(x_to_user):
+                    target.setx(x - 10)
+                else:
+                    target.sety(y + 10)
+            else:
+                if abs(y_to_user) > abs(x_to_user):
+                    target.setx(x - 10)
+                else:
+                    target.sety(y - 10)
+        else:
+            a = random.randint(1,5)
+            if a == 1:
+                target.sety(y-10)
+            elif a == 2:
+                target.sety(y+10)
+            elif a == 3:
+                target.setx(x-10)
+            elif a == 4:
+                target.setx(x+10)
+    t.clear()
+    t.write("PLAYER : " + player +
+            "\nSCORE : " + str(tryCount) +
+            "\nPOSITION : " + str(user.position()) +
+            "\nTARGET : " + str(target.position()))
+
 
 
 def gameturn():
@@ -113,6 +207,7 @@ def gameturn():
         user.clear()
         if tryCount%3 == 0 and beforemoved != tryCount:
             targetmove()
+            time.sleep(0.1)
             targetmove()
             beforemoved = tryCount
         screen.onkeypress(move_right, "Right")
@@ -152,14 +247,38 @@ if __name__ == '__main__':
     user = user()
     target = target()
     screen = init()
+    while True:
+        while stagecount < 5:
+            player = getname()
+            score = gameturn()
 
-    while stagecount < 5:
-        player = getname()
-        score = gameturn()
+            scores.append([player, score])
+            stagecount += 1
 
-        scores.append([player, score])
-        stagecount += 1
-
-    scores.sort(key=lambda x: x[1])
-    print(scores)
+        scores.sort(key=lambda x: x[1])
+        print(scores)
+        t.clear()
+        t.write("1st : " + scores[0][0] + " score : " + str(scores[0][1]) +
+                "\n2nd : " + scores[1][0] + " score : " + str(scores[1][1]) +
+                "\n3rd : " + scores[2][0] + " score : " + str(scores[2][1]) +
+                "\n4th : " + scores[3][0] + " score : " + str(scores[3][1]) +
+                "\n5th : " + scores[4][0] + " score : " + str(scores[4][1]) +
+                "\nplease go to target to restart")
+        target.color("red")
+        target.speed(0)
+        target.penup()
+        target.goto(100,100)
+        while True:
+            user.clear()
+            screen.onkeypress(move_right, "Right")
+            screen.onkeypress(move_left, "Left")
+            screen.onkeypress(move_up, "Up")
+            screen.onkeypress(move_down, "Down")
+            if user.distance(target) < 1:
+                user.reset()
+                screen.reset()
+                target.reset()
+                scores = []
+                stagecount = 0
+                break
 
